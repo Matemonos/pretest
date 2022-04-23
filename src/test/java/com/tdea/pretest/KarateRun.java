21adff7bde81d0d18fa65379b7f7923511325935
Mateo A.C <82480127+mateoAristizabalCampuzano@users.noreply.github.com>
2022-04-23 11:44:52 -0500

    Practica de Karate para tests
    aplicamos karate a el pretest, escenarios de crear y logear usuario

---
+++ src/test/java/com/tdea/pretest/KarateRun.java
@@ -0,0 +1,12 @@
+package com.tdea.pretest;
+
+
+import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.Test;
+
+public class KarateRun {
+    @Karate.Test
+    Karate testKarate(){
+        return Karate.run("classpath:com.tdea.pretest/karate");
+    }
+}
